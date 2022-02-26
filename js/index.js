a9daec6c764b6da234ffadf561a2aec8ae6e64b1
Gail Mukungurutse <gail@mozzington.com>
2022-02-26 18:24:28 +0000

    created and mapped cards

--- js/index.js
+++ js/index.js
@@ -1,37 +1,47 @@
 import { dates } from "./data.js";
+ 
+const timeline = document.querySelector(".timeline")
+dates.map((item)=> {
+    const card = document.createElement("div");
+    card.classList.add("timeline-item");
+    timeline.appendChild(card);
+
+    const titleElement = document.createElement("h2");
+    titleElement.classList.add("timeline-item-title");
+    titleElement.textContent = item.title;
+
+    const dateElement = document.createElement("p");
+    dateElement.textContent = item.date;
+
+    // const FullDescription = document.createElement("p");
+    // FullDescriptionElement.textContent = FullDescription;
+
+    // const imageElement = document.createElement("img");
+    // imageElement.setAttribute("src", image);
+
+    const summaryElement = document.createElement("p");
+    summaryElement.textContent = item.summary
+    const moreInfo = document.createElement("button");
+    moreInfo.textContent = "READMORE"
+    card.append(titleElement, dateElement, summaryElement, moreInfo);
+})
+/**
+ *       <div class="timeline-item">
+          <h2 class="timeline-item-title"></h2>
+          <span class="timeline-item-date"> </span>
+          <p class="timeline-item-summary"></p>
+
+               <button class= "timeline-item-more-info">
+               <div id = "modal-container">
+                  <h2 id="modal-title"></h2>
+                  <span id="modal-date"> </span>
+                  <img id="modal-image">
+                 <p id="modal-full-description"></p>
+                 <button id="modal-close-button">Minimise</button>
+          </div> <!-- end of modal-container -->  
+         </button>
+<div> <!-- end of timeline-item  -->
+
+ */
 
 
-
-
-for (let i = 0; i < dates.length; i++) {
-    const timeline = document.querySelector(".timeline");
-    const newDiv = document.createElement("div");
-
-    newDiv.setAttribute("class", "timeline-item");
-
-    const timelineDate = document.createElement("span");
-    timelineDate.textContent = dates[i].date;
-    timelineDate.setAttribute("class", "date");
-  
-    newDiv.appendChild(timelineDate);
-
-    const h2 = document.createElement("h2");
-    h2.setAttribute("class" ,"time-timeline-title");
-    h2.textContent = dates[i].title;
-    newDiv.appendChild(h2);
-
-    const span = document.createElement("span");
-    span.setAttribute("class" ,"time-timeline-date");
-    span.textContent = dates[i].date;
-    newDiv.appendChild(span);
-
-    const text = document.createElement("p")
-    text .setAttribute("class" ,"time-timeline-summary");
-    text.textContent = dates[i].summary;
-    newDiv.appendChild(text);
-
-    const openButton = document.createElement("button");
-    openButton.setAttribute("class" ,"time-timeline-more-info");
-    openButton.textContent = "More Info";
-    newDiv.appendChild(openButton);
-}
